504b6ccf9ce7263b2b75a0af4c0d2aa307290a60
prisae <mail@werthmuller.org>
2017-04-03 10:06:39 -0500

    GPR scripts for re-submission

---
+++ notebooks/gpr-create-data.py
@@ -0,0 +1,139 @@
+"""
+Creata GPR data
+
+*This notebook is to reproduce the results from the article. For more
+information see the article in the parent directory.*
+
+=> The figure is created in the notebook `gpr-figure.ipynb`.
+
+Warning!
+--------
+**SciPy 0.19.0 seems to have a memory leak in quadpack. Use another version,
+here I used 0.18.1.**
+"""
+
+import os
+import scipy
+import subprocess
+import numpy as np
+
+from empymod.model import gpr, tem
+from empymod.utils import printstartfinish
+
+
+# Parameters
+# Parameters as in Hunziker et al., 2015
+
+x = np.r_[0.001, np.arange(1, 201)*.02]  # X-coord from 0-4 m, spacing of 20 cm
+y = np.zeros(x.size)                     # Y-coord = 0
+zsrc = 0.0000001                         # Source depth just slightly below 0
+zrec = 0.5                               # Receiver depth at 0.5 m
+depth = [0, 1]                           # 1 layer of 1 m thickness between two
+                                         #          half-spaces
+eperm = [1, 9, 15]                       # El. permit. model
+res = [1e23, 200, 20]                    # Resistivity model: air, 1st layer,
+                                         #                    2nd layer
+f = np.arange(1, 850+1)*1e6              # Frequencies from 1e6 Hz to 850 Hz,
+                                         #                  1e6 Hz sampling
+t = np.arange(321)/4*1e-9                # Times from 1 ns to 80 ns,
+                                         #            4 samples per ns
+cf = 250e6                               # Center frequency
+verb = 2                                 # Verbosity level
+
+# Collect general input parameters
+inp = {'src': [0, 0, zsrc], 'rec': [x, y, zrec], 'depth': depth, 'res': res,
+       'cf': cf, 'ab': 11, 'gain': 3, 'epermH': eperm, 'epermV': eperm, 'loop':
+       'off', 'verb': verb, 'freqtime': t, 'opt': 'spline', 'ft': 'fft',
+       'ftarg': [f[0], f.size, 2048]}
+       # FFT: we are padding with zerose to 2048 samples
+
+
+# Calculate GPR with `empymod` for FHT, QWE, and QUAD and store it in
+# `*.npy`-files which are loaded in the `gpr-figures.ipynb`.
+
+# 1. FHT
+gprFHT = gpr(ht='fht', htarg=['key_401_2009', 100], **inp)
+np.save('data/GPR-FHT', gprFHT)
+
+
+# 2. QWE
+if scipy.__version__ == '0.19.0':
+    print('SciPy 0.19.0 has a memory leak in QUAD, use another version!')
+gprQWE = gpr(ht='qwe', htarg=[1e-8, 1e-15, '', 200, 200, 60, 1e-6, 160, 4000],
+             **inp)
+np.save('data/GPR-QWE', gprQWE)
+
+
+# 3. QUAD
+if scipy.__version__ == '0.19.0':
+    print('SciPy 0.19.0 has a memory leak in QUAD, use another version!')
+gprQUA = gpr(ht='quad', htarg=['', '', 51, '', 160, 500], **inp)
+np.save('data/GPR-QUA', gprQUA)
+
+
+# Calculate GPR with `EMmod`
+# To calculate the `EMmod`-result, `EMmod` must be installed and in the
+# bash-PATH.
+
+# We use the empymod-utility to measure execution time; get start time
+tstart = printstartfinish(verb)
+
+# Change directory
+os.chdir('data/GPR')
+
+# Run EMmod
+subprocess.run('bash gprloop_twointerface.scr', shell=True,
+               stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+
+# Change back to original directory
+os.chdir('../..')
+
+# Read data
+fEM = np.zeros((f.size, x.size), dtype=complex)
+for i in range(f.size):
+    filename = 'data/GPR/gprloop_twointmod_freq'+str(i+1)+'_11.bin'
+    tf = open(filename, 'rb')
+    temp = np.fromfile(tf)
+    fEM[i, :] = temp[x.size*2:x.size*4:2] + 1j*temp[x.size*2+1:x.size*4+1:2]
+
+# Multiply with ricker wavelet
+cfc = -(np.r_[0, f[:-1]]/250e6)**2
+fwave = cfc*np.exp(cfc)
+fEM *= fwave[:, None]
+
+# Do f->t transform
+tEM, conv = tem(fEM, x, f, t, 0, 'fft', [f[0], f.size, 2048, None])
+
+# Apply gain; make pure real
+tEM *= (1 + np.abs((t*10**9)**3))[:, None]
+gprEMmod = tEM.real
+
+# Print execution time (it will show 'empymod', but obviously in this case it
+# is `EMmod`)
+printstartfinish(verb, tstart)
+
+# Store EMmod
+np.save('data/GPR-EMmod', gprEMmod)
+
+
+# Calculate theoretical arrival times
+
+# Arrival times for direct wave
+clight = 299792458
+vel = clight/np.sqrt(eperm[1])
+arrtime = np.sqrt((zsrc-zrec)**2 + x**2 + y**2)/vel
+
+# Arrival times for reflected wave
+arrtimeref = np.sqrt((np.abs(zsrc - depth[1]) + np.abs(zrec - depth[1]))**2 +
+                     x**2 + y**2)/vel
+
+# Arrival times for refracted wave in the air
+# This only works if ypos = 0
+refractang = np.arcsin(vel/clight)
+arrtimerefair = (np.abs(zsrc - depth[0])/np.cos(refractang) +
+                 np.abs(zrec - depth[0])/np.cos(refractang))/vel
+arrtimerefair += (np.abs(x) - np.abs(zsrc - depth[0])*np.tan(refractang) -
+                  np.abs(zrec - depth[0])*np.tan(refractang))/clight
+
+np.savez('data/ArrivalTimes.npz', arrtime=arrtime, arrtimeref=arrtimeref,
+         arrtimerefair=arrtimerefair, x=x)
---
+++ notebooks/gpr-figure.py
@@ -0,0 +1,159 @@
+"""
+GPR figure
+
+*This notebook is to reproduce the results from the article. For more
+information see the article in the parent directory.*
+
+=> The data is calculated in the notebook `gpr-create-data.ipynb`.
+"""
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+# Style adjustments
+mpl.rcParams['figure.dpi'] = 300
+mpl.rcParams['savefig.dpi'] = 300
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams['font.serif'] = 'Computer Modern Roman'
+mpl.rcParams['font.family'] = 'serif'
+mpl.rcParams['font.style'] = 'normal'
+
+
+# Load data
+
+# 1. FHT
+gprFHT = np.load('data/GPR-FHT.npy')
+
+# 2. QWE
+gprQWE = np.load('data/GPR-QWE.npy')
+
+# 3. QUAD
+gprQUA = np.load('data/GPR-QUA.npy')
+
+# 4. EMmod
+gprEMmod = np.load('data/GPR-EMmod.npy')
+
+# 5. Arrival times
+x = np.load('data/ArrivalTimes.npz')['x']
+arrtime = np.load('data/ArrivalTimes.npz')['arrtime']
+arrtimeref = np.load('data/ArrivalTimes.npz')['arrtimeref']
+arrtimerefair = np.load('data/ArrivalTimes.npz')['arrtimerefair']
+
+
+# Plot
+
+fig, axs = plt.subplots(figsize=(10, 2.5), facecolor = 'w', nrows=1, ncols=6)
+fig.subplots_adjust(wspace=.05)
+axs = axs.ravel()
+
+# # 1. Plot model # #
+plt.subplot2grid((1, 6), (0, 0), colspan=2)
+
+# Some settings
+arrow_prop = {'length_includes_head': True, 'zorder': 5, 'joinstyle': 'round',
+              'capstyle': 'round', 'head_width': 0.2, 'head_length': 0.4}
+x1 = 1
+x0 = 1.2
+y0 = 2
+y1 = 3.5
+y2 = 5
+
+# Plot first interface
+plt.plot([x0, 9.5], [y0, y0], 'k', lw=1)
+plt.plot(2, y0+.05, 'k*', lw=2, label='Source')
+plt.text(x1, y0, r'$0\,$m', horizontalalignment='right', fontsize=8)
+plt.text(x1, y0-1, r'$\sigma=0\,$S/m and $\epsilon_{\rm{r}}=1$ (air)',
+         fontsize=8)
+
+# Plot second interface
+xx = np.linspace(1.5, 9.2, 12)
+plt.plot(xx, y1*np.ones(xx.shape), 'kv', ms=4, label='Receiver')
+plt.text(x1, y1, r'$0.5\,$m', horizontalalignment='right', fontsize=8)
+plt.text(x1, y1+1,
+         r'$\sigma=0.005\,$S/m and $\epsilon_{\rm{r}}=9$ (1st sediment)',
+         fontsize=8)
+
+# Plot third interface
+plt.plot([x0, 9.5], [y2, y2], 'k', lw=1)
+plt.text(x1, y2, r'$1\,$m', horizontalalignment='right', fontsize=8)
+plt.text(x1, y2+1,
+         r'$\sigma=0.005\,$S/m and $\epsilon_{\rm{r}}=15$ (2nd sediment)',
+         fontsize=8)
+
+# Plot Arrows
+plt.plot([-1, -2], [y0, y0], 'C3', solid_capstyle='round', lw=1,
+         label='Direct wave')
+plt.arrow(2, 2.05, 5.1, y1-2.05, color='C3', **arrow_prop)
+
+plt.plot([2, 2.2, 6], [y0+.05, y0, y0], 'C8', solid_capstyle='round', lw=1,
+         zorder=5, label='Refracted at the surface')
+plt.arrow(6, y0, 1.1, y1-y0-.05, color='C8', **arrow_prop)
+
+plt.plot([2, 2+10.2/3], [2.05, y2], 'C2', solid_capstyle='round', lw=1,
+         label='Reflected at subsurface interface')
+plt.arrow(2+10.2/3, y2, 5.1/3, -y1+2.05, color='C2', **arrow_prop)
+
+# General plot settings
+plt.grid('off')
+plt.axis('off')
+plt.xticks([])
+plt.yticks([])
+plt.xlim([-.5, 10])
+plt.ylim([9, 1.5])
+
+# Legend
+plt.legend(loc='lower center', fontsize='small', bbox_to_anchor=(0.5, -.2))
+
+
+# # Plot GPR result # #
+params = {'cmap':'gray', 'vmin':-5*10**12, 'vmax':5*10**12,
+          'interpolation':'sinc', 'extent':[0, 4, 80, 0], 'aspect':'auto'}
+
+# Set axis and limits
+def limits(i):
+    plt.sca(axs[i+2])
+    ax = plt.gca()
+    plt.plot(x, arrtimerefair*10**9, 'C8', lw=1.5)
+    plt.plot(x, arrtime*10**9, 'C3', lw=1.5)
+    plt.plot(x, arrtimeref*10**9, 'C2', lw=1.5)
+    plt.ylim([80, 0])
+    plt.xlim([0, 4])
+    plt.xlabel('Offset (m)')
+    plt.xticks(np.arange(4))
+    ax.set_yticklabels([], ())
+    plt.grid('off')
+    return ax
+
+# Plot EMmod
+limits(0)
+plt.title(r'EMmod \\ 9\,h 30\,min')
+plt.imshow(gprEMmod, **params)
+
+# Plot QUAD
+limits(1)
+plt.title(r'QUAD \\ 7\,h 46\,min')
+plt.imshow(gprQUA, **params)
+
+# Plot QWE
+limits(2)
+plt.title(r'QWE \\ 4\,h 22\,min')
+plt.imshow(gprQWE, **params)
+
+# Plot FHT
+ax = limits(3)
+plt.title(r'FHT \\ $<$ 1\,min')
+plt.imshow(gprFHT, **params)
+plt.xticks(np.arange(5))
+ax.yaxis.set_ticks_position('right')
+ax.yaxis.set_label_position('right')
+plt.ylabel('Time (ns)')
+
+# Plot colorbar
+cax, kw = mpl.colorbar.make_axes([axs[2], axs[3], axs[4], axs[5]],
+                                 location='right', pad=.1)
+cb = plt.colorbar(cax=cax, **kw)
+
+# Save figure and show it
+plt.savefig('../figures/gpr.jpg', bbox_inches='tight')
+plt.show()
